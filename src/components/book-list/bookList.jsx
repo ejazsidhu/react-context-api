8c0999193f78cb413afa0de32a3bc39b6647790b
ejazsidhu <ejazsidhu@gmail.com>
2020-04-16 17:35:26 +0500

    book list component added

---
+++ src/components/book-list/bookList.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import styles from './bookList.module.css';
+class BookList extends Component {
+
+    render() {
+        return (
+            <div className={styles.bookList}>
+                <ul>
+                    <li>
+
+                    </li>
+                    <li>
+
+                    </li>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default BookList;
